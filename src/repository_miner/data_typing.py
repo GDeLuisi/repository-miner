99c41e20e6ae9cd37100f4366149bc3901e18294
GDeLuisi <deluisigerardo@gmail.com>
2025-05-07 09:12:20 +0200

    Introduced mining utilities for tree,authors and commit discovery

--- src/repository_miner/data_typing.py
+++ src/repository_miner/data_typing.py
@@ -1,9 +1,10 @@
 from  dataclasses import dataclass,field
-from datetime import date
+from datetime import datetime
 from time import strptime
 from typing import Literal,get_args,Iterable,Optional,Union,Generator,Callable
 import json
 from pathlib import Path
+from repository_miner.utility import Call
 
 @dataclass
 class Author():
@@ -30,15 +31,28 @@ class CommitInfo():
     subject:str
     author_name:str
     author_email:str
-    date:date
+    date:datetime
     def __hash__(self):
         return hash(self.commit_hash)
+    def get_tree(self)->'Tree':
+        raise NotImplementedError()
+
 @dataclass
 class Head():
     name:str
     hash:str
     def __hash__(self):
         return hash(self.hash)
+    def traverse_commits(self)->Generator[CommitInfo,None,None]:
+        raise NotImplementedError()
+    
+class HeadImpl(Head):
+    def __init__(self,name:str,hash:str,retrieve_func:Call):
+        super().__init__(name,hash)
+        self.retrieve_func=retrieve_func
+    def traverse_commits(self):
+        return self.retrieve_func()
+
 
 @dataclass
 class Blob():
@@ -48,18 +62,45 @@ class Blob():
     size:int
     def __hash__(self):
         return hash(self.hash)
+    def get_source(self)->list[str]:
+        raise NotImplementedError()
 
 @dataclass
 class Tree():
     hash:str
-    def iterate_tree(self):
+    path:str
+    def traverse(self)->Generator[Union['Tree',Blob],None,None]:
         raise NotImplementedError()
     def __hash__(self):
         return hash(self.hash)
     
 class TreeImpl(Tree):
-    def __init__(self,hash:str,iter_function:Callable[...,Generator[Union[Tree,Blob],None,None]]):
-        super().__init__(hash)
+    def __init__(self,hash:str,path:str,iter_function:Call):
+        super().__init__(hash,path)
         self.iter_func=iter_function
-    def iterate_tree(self)->Generator[Union[Tree,Blob],None,None]:
+    def traverse(self)->Generator[Union[Tree,Blob],None,None]:
         return self.iter_func()
+    
+class CommitInfoImpl(CommitInfo):
+    def __init__(self
+    ,commit_hash:str
+    ,abbr_hash:str
+    ,tree:str
+    ,refs:str
+    ,subject:str
+    ,author_name:str
+    ,author_email:str
+    ,date:datetime
+    ,tree_func:Call):
+        super().__init__(commit_hash,abbr_hash,tree,refs,subject,author_name,author_email,date)
+        self.tree_func=tree_func
+    
+    def get_tree(self)->Tree:
+        return self.tree_func()
+    
+class BlobImpl(Blob):
+    def __init__(self,hash:str,name:str,path:str,size:int,source_func:Call):
+        super().__init__(hash,name,path,size)
+        self.source_func=source_func
+    def get_source(self):
+        return self.source_func()
--- tests/test_miner.py
+++ tests/test_miner.py
@@ -5,16 +5,22 @@ from pytest import fixture
 from pathlib import Path
 from subprocess import check_output
 main_path=Path.cwd()
+test_path=main_path.parent.joinpath("pandas")
 @fixture
 def git():
     return RepoMiner(main_path.as_posix())
 
 def test_log(git):
-    t=execute_command(cmd_builder("log",main_path.as_posix()))
-    res=git.log()
+    res=git.retrieve_commits()
+    t=check_output(f"git -C {main_path.as_posix()} log --pretty=format:%H",text=True,shell=True).splitlines()
+    for i,c in enumerate(res):
+        assert c.commit_hash == t[i]
+    
+def test_count(git):
+    res=git.n_commits()
+    t=int(execute_command(cmd_builder("rev-list",main_path.as_posix(),"HEAD","--count")))
     assert t==res
-    # print(res)
-
+    
 def test_local_branches(git):
     res=check_output(f"git -C {main_path.as_posix()} branch -l",text=True,shell=True).split("\n")[:-1]
     res=list(map(lambda a: a.strip("*").strip(),res))
@@ -25,3 +31,18 @@ def test_local_branches(git):
     assert len(res)==len(heads)
     assert set(res)==set(map(lambda h:h.name,heads))
     assert set(res_hashes)==set(map(lambda h:h.hash,heads))
+    for head in heads:
+        res=len(check_output(f"git -C {main_path.as_posix()} log {head.name} --pretty='format:%h'",text=True,shell=True).splitlines())
+        assert res==len(list(head.traverse_commits()))
+
+def test_tree(git):
+    tree = git.tree("HEAD")
+    t=check_output(f"git -C {main_path.as_posix()} ls-tree HEAD -r -t --format=%(objectname)",text=True,shell=True).split("\n")[:-1]
+    t.sort()
+    traverse=list(tree.traverse())
+    traverse.sort(key=lambda a:a.hash)
+    t_hash=[i.hash for i in traverse]
+    assert t_hash  == t
+        
+def test_author(git):
+    git.authors()
